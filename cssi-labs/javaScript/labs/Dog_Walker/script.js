d33448d4722bc254265e68113ab738bfa82d33e5
Alexander Garcia <alexnlt9@gmail.com>
2019-07-09 09:32:28 -0700

    Dog walker lab completed

--- cssi-labs/javaScript/labs/Dog_Walker/script.js
+++ cssi-labs/javaScript/labs/Dog_Walker/script.js
@@ -17,17 +17,40 @@ const dogName1 = "Steve";
 const dogType1 = "beagle";
 
 // Complete Task 1 Below
-
-
+console.log("TASK 1 BELOW");
+console.log("I will walk " + dogName1 + " today at 12:00pm");
 
 const dogName2 = "Joe";
 const dogType2 = "bulldog";
 
 // Complete Task 2 Below
-
-
+console.log("TASK 2 BELOW");
+if (dogType2 === "corgi") {
+    console.log("I will walk " + dogName2 + " today at 12:00pm");
+} else {
+    console.log("I will walk " + dogName2 + " today at 1:00pm");
+}
 
 const dogName3 = "Lola";
 const dogType3 = "poodle";
 
 // Complete Task 3 Below
+console.log("TASK 3 BELOW");
+function advancedSchedule(dogType, dogName) {
+    if (dogType === "corgi" || dogType === "beagle") {
+        console.log("I will walk " + dogName + " today at 12:00pm");
+    } else if (dogType === "bulldog") {
+        console.log("I will walk " + dogName + " today at 1:00pm");
+    } else {
+        console.log("I will walk " + dogName + " today at 2:00pm");
+    }
+}
+advancedSchedule(dogType1, dogName1);
+advancedSchedule(dogType2, dogName2);
+advancedSchedule(dogType3, dogName3);
+
+// Complete Extension Below
+// console.log("EXTENSION BELOW");
+// function favoriteDog() {
+
+// }
